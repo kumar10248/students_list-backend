d613a55e627ec751bb07de24eb5dc102eb2e89f3
kumar10248 <kumardevashish000@gmail.com>
2025-01-02 17:04:47 +0530

    chore: Rename MONGO_URL to MONGO_URI for consistency in environment variables

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const url=process.env.MONGO_URL||"mongodb://localhost:27017/StudentDB";
+const url=process.env.MONGO_URI||"mongodb://localhost:27017/StudentDB";
 mongoose.connect(url).then(()=>{console.log("Connection Created Successfully")}).catch(error=>console.error(error));
 module.exports=mongoose;
